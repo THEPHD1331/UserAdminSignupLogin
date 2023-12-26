ea0b8a462583c8b42a533a1bcb9de33be138f2e5
Paras Dongre <parasdarpan39@gmail.com>
2023-12-26 14:56:21 +0530

    initial commit

---
+++ src/main/java/com/userapp/service/UserService.java
@@ -0,0 +1,11 @@
+package com.userapp.service;
+
+import com.userapp.dto.UserDto;
+import com.userapp.model.User;
+
+public interface UserService {
+	
+	User save (UserDto userDto);
+	
+
+}
---
+++ src/main/java/com/userapp/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.userapp.service;
+
+import com.userapp.dto.UserDto;
+import com.userapp.model.User;
+import com.userapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User save(UserDto userDto) {
+		User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()) , userDto.getRole(), userDto.getFullname());
+		return userRepository.save(user);
+	}
+
+}
